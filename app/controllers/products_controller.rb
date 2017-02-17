3d1529da6890af6720808184b5e983f57f395766
MonKoDev Technologies <gmalugani@gmail.com>
2017-02-17 10:20:29 -0300

    crea la ruta /product/:id/edit

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -20,6 +20,10 @@ class ProductsController < ApplicationController
 		
 	end
 
+	def edit
+		@product = Product.find(params[:id])	
+	end
+
 	private
 		def product_params
 			params.require(:product).permit(:name, :url, :description)			
