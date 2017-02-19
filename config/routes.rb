cf98718669a5a8bccb0140bb92b10f1531d96262
MonKoDev Technologies <gmalugani@gmail.com>
2017-02-18 23:52:25 -0300

    Implementa autenticacion usuarios

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
-  resources :users, only: [:new, :create]
+    root 'products#index'
+
+    get 'login', to: 'sessions#new'
+    post 'login', to: 'sessions#create'
+
     get '/products', to: 'products#index'
     get '/products/new', to: 'products#new', as: 'new_product'
     post '/products', to: 'products#create'
@@ -7,8 +11,8 @@ Rails.application.routes.draw do
     get '/products/:id/edit', to: 'products#edit' , as: 'edit_product'
     patch '/products/:id', to: 'products#update'
     delete '/products/:id', to: 'products#destroy'
-
-    root 'products#index'
+    resources :users, only: [:new, :create]
+   
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
